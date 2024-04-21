9b148deff93f99fa177e500532692fe4df13f198
Chad Powell <chada.powell@outlook.com>
2024-04-21 17:48:35 -0400

    update css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ export const About = () => {
   return (
     <div className='about'>
         <h4 className='about-title'>About</h4>
-        <p className='about-content'>{character.bio}</p>
+        <h5 className='about-content'>{character.bio}</h5>
     </div>
   )
 }
